cf03e570e2a0379ad7df7c3520755ee4059f4d91
RupaDey404 <rupa121353@gmail.com>
2025-11-02 15:41:52 +0600

    Form filling test data is added for name

---
+++ FormTestData.java
@@ -0,0 +1,6 @@
+package SeleniumProject;
+
+public class FormTestData {
+    public static final String f_name_path = "//input[@id='firstName']";
+    public static final String l_name_path = "//input[@id='lastName']";
+}
