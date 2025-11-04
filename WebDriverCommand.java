01383106fd9f4ed5d2937999d72a9272653d41fd
RupaDey404 <rupa121353@gmail.com>
2025-11-04 13:20:03 +0600

    practice-1 is created for browser cmd

--- WebDriverCommand.java
+++ WebDriverCommand.java
@@ -3,6 +3,7 @@ package BeginnerToAdvancedAutomation;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class WebDriverCommand {
@@ -18,16 +19,51 @@ Get Page Source (HTML Source code) and Page Source length
 Print Page Length on Eclipse Console.
 Close the Browser.
   */
+    static WebDriver driver;
+    @BeforeMethod
+    void initDriver(){
+        //1. Launch new chrome driver
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
     @Test
     void WebDriverCommand1() throws InterruptedException{
 
-        WebDriverManager.chromedriver().setup();
+//      -----------------------------------------
+//
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.get("https://www.google.com");
+//        driver.close();
+//
+//        System.out.println("chromedriver is running");
 
-        WebDriver driver = new ChromeDriver();
+//        ---------------------------------------
+        //2.
+//        String url = "https://www.shop.demoqa.com";
+        String url = "https://demoqa.com/";
+        driver.get(url);
+        Thread.sleep(2000);
+        //3.
+        String pageTitle = driver.getTitle();
+        System.out.println("Website title is :" + pageTitle);
+         //4.
+        int titleLength = pageTitle.length();
+        System.out.println("page title length is : "+titleLength);
 
-        driver.get("https://www.google.com");
+        //5.
+       String actualUrl = driver.getCurrentUrl();
+        System.out.println("actual url: "+actualUrl);
+        if (actualUrl.equals(url)) System.out.println("verification successful and opened corrected browser");
+        else System.out.println("verification unsuccessful");
+        Thread.sleep(2000);
+        //6.7.
+        String pageSource = driver.getPageSource();
+        int pageSrcLength = pageSource.length();
+        System.out.println("page source length "+pageSrcLength);
+        //8.
         driver.close();
 
-        System.out.println("chromedriver is running");
     }
 }
