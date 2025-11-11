9bb37204261b5970a7c15aebb077899bc8042131
RupaDey404 <rupa121353@gmail.com>
2025-11-11 13:03:57 +0600

    WebElement command is added those are click(), sendKeys, isDisplayed

--- WebElementCommand.java
+++ WebElementCommand.java
@@ -22,9 +22,35 @@ public class WebElementCommand {
        driver.get(url);
        Thread.sleep(500);
         driver.navigate().to("https://demoqa.com/text-box");
-        WebElement element = driver.findElement(By.xpath("//input[@id='userName']"));
-        element.click();
+        WebElement username = driver.findElement(By.xpath("//input[@id='userName']"));
+        username.click();
         Thread.sleep(500);
+        username.sendKeys("Rupa");
+        Thread.sleep(500);
+
+        WebElement email = driver.findElement(By.xpath("//input[@id='userEmail']"));
+        email.click();
+        email.sendKeys("rupa1234@gmail.com");
+        Thread.sleep(500);
+
+        WebElement cur_address = driver.findElement(By.xpath("//textarea[@id='currentAddress']"));
+        cur_address.click();
+        cur_address.sendKeys("Dhaka");
+        Thread.sleep(500);
+
+        WebElement per_address = driver.findElement(By.xpath("//textarea[@id='permanentAddress']"));
+        per_address.click();
+        per_address.sendKeys("Dhaka");
+        Thread.sleep(500);
+
+        WebElement submitBtn = driver.findElement(By.xpath("//button[@id='submit']"));
+        submitBtn.click();
+        Thread.sleep(1000);
+
+        System.out.println("submitted data successfully");
+
+        boolean status = username.isDisplayed();
+        System.out.println(status);
        driver.quit();
     }
 }
